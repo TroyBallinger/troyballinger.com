385b00f969f64e696f1e2aeb77fd65249511c17c
Troy <troy@iconoclasm.re>
2020-02-10 01:59:34 -0500

    consolidated to one page, layout compatibility improvements

--- scroll.js
+++ scroll.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-		document.getElementById("header_link_holder").style.paddingTop = "0px";
-		document.getElementById("header_link_holder").style.paddingBottom = "0px";
+		document.getElementById("header_link_holder").style.paddingTop = "0";
+		document.getElementById("header_link_holder").style.paddingBottom = "0";
 	} else {
-		document.getElementById("header_link_holder").style.paddingTop = "10px";
-		document.getElementById("header_link_holder").style.paddingBottom = "10px";
+		document.getElementById("header_link_holder").style.paddingTop = "1vh";
+		document.getElementById("header_link_holder").style.paddingBottom = "1vh";
 	}
 }
