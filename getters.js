101a7a7bd9b598979d5fd3f9ab6c8fa53720185e
Troy Ballinger <troy@shippabo.com>
2021-12-21 01:17:43 -0800

    initial commit

---
+++ getters.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', (_event) => {
+	getWebsiteLastUpdatedDate();
+	getPublicRepos();
+});
+
+async function getWebsiteLastUpdatedDate() {
+	fetch('https://api.github.com/repos/TroyBallinger/troyballinger.github.io/commits/main')
+	.then(res => res.json())
+	.then(latestCommitData => {
+		// Get date of last commit
+		const date = latestCommitData['commit']['author']['date'].split('T')[0];
+
+		document.getElementById('last-updated').innerHTML = `Last updated: ${date}`;
+	}).catch(_err => {
+		// GitHub API error or parse error
+	});
+}
+
+// Pull every public repository from my profile
+async function getPublicRepos() {
+	fetch('https://api.github.com/users/TroyBallinger/repos')
+	.then(res => res.json())
+	.then(reposArray => {
+		const projectsList = document.getElementById('projects-list');
+
+		reposArray.forEach(repo => {
+			const newBullet = document.createElement('li');
+			const repoLink = document.createElement('a');
+
+			const { html_url, name } = repo;
+
+			try {
+				repoLink.href = html_url;
+				repoLink.innerHTML = name;
+			} catch (_err) {
+				// Parse error
+				return;
+			}
+
+			projectsList.appendChild(newBullet);
+			newBullet.appendChild(repoLink);
+		});
+
+		// Parsing error(s) or no public repositories
+		if (projectsList.childElementCount == 0) {
+			document.getElementById('projects').style.display = 'none';
+		}
+	}).catch(_err => {
+		// GitHub API error or population failure
+	});
+}
